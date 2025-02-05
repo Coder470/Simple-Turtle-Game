c35a56ce70ede00d9b2e54dd14895fbd11ebd7c6
Coder470 <Coder472.team20@gmail.com>
2025-02-05 20:53:27 +0000

    Turtle_Game.py

--- Turtle_Game.py
+++ Turtle_Game.py
@@ -1,1 +1,24 @@
+from turtle import*
 
+def turtle_model():
+    color("blue")
+    shape("turtle")
+    pensize(4)
+
+def turtle_speed():
+    speed(1)
+
+def turtle_path():
+    forward(50)
+    right(90)
+    forward(50)
+    right(90)
+    forward(50)
+    right(90)
+    forward(50)
+
+
+#Game execution
+turtle_model()
+turtle_speed()
+turtle_path()
